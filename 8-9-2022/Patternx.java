928a08e84d08e4ad4bc24a1d91d894be462a01ce
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-09-08 14:53:59 +0530

    Add files via upload
    java code

---
+++ 8-9-2022/Patternx.java
@@ -0,0 +1,44 @@
+/*
+   *       * 
+    *     *  
+     *  *
+       * 
+     *   *
+   *      *
+ *         *
+*/
+ class PatternX
+ 	
+
+{
+	public static void main(String[] args)
+    	
+		{
+			for( int a =0; a<=0; a++)
+		{
+			for( int b = 0; b<=0; b++) 
+  				{
+					System.out.print("*");
+	
+                                    if(a%1==0)
+					break;
+				}
+			System.out.println();
+			
+			}
+	}
+
+
+
+}
+/*
+
+
+1	 *     *
+2	  *   *
+3 	   * *
+4	    *
+5	   *  *
+6         *    *
+7        *      *
+*/
