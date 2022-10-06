58a7020acbc569eb041087ec4a7f51d5f6ceeaca
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-10-06 21:13:12 +0530

    Add files via upload

---
+++ 6-10-2022/Child.java
@@ -0,0 +1,22 @@
+
+//inherit the property of parent class  with method void speak and dance;
+// Author: Mane Manisha.R.
+package corejava;
+public class Child extends Parent{
+	 public Child(String speak)
+	 {
+		super(speak);
+	 }	// using Method to create ;
+      public static void main(String[] args) 
+     {
+	System.out.println( "speak:"+"let's start");
+   
+    	  System.out.println( "dance:"+"ding dong ding");
+      }
+
+	}
+
+    
+
+
+	
