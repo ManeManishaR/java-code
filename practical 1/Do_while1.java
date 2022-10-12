76ab486d52588f71e1df702082850492bd796046
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-10-12 14:28:53 +0530

    Add files via upload

---
+++ practical 1/Do_while1.java
@@ -0,0 +1,23 @@
+package corejava;
+import java.util.Scanner;
+public class Do_while1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the no:");
+		int i = sc.nextInt();	
+		System.out.println("Enter the no:");
+		int j= sc.nextInt();
+	
+
+		// take statement
+	do
+	{
+		System.out.println(j+"");
+		j--;
+		i--;
+	}
+	//check condition.
+	while(i>=1); 
+	     }
+	    }
+
---
+++ practical 1/While1.java
@@ -0,0 +1,33 @@
+//Create program to while loop.
+//Author Mane Manisha.R
+package corejava;
+import.java.util.Scanner
+public class While1 {
+	public static void main(String[] args) {
+	Scanner sc=new .Scanner(System.in);
+	while(true) {
+		System.out.println("Enter the no:");
+       int i = sc.nextInt();
+		// Text condition.
+		
+		if(i<0) {
+			//check condition	
+			System.out.println("false");
+			
+			break;
+		    }   
+	}
+}
+}
+
+			
+
+		
+		
+	
+	
+
+
+	
+
+
