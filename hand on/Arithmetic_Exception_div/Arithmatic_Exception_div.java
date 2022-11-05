7677cff72cffe7adf12c10facafe52b8c7775c22
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-11-05 15:04:15 +0530

    Add files via upload

---
+++ hand on/Arithmetic_Exception_div/Arithmatic_Exception_div.java
@@ -0,0 +1,39 @@
+/*WAP to  ArithmeticException  divide by 0 to any Number.
+ * Mane Manisha .R
+ * 4-11-2022
+ * 
+ */
+package Exception;
+ class Arithmatic_Exception_div {
+       public static void main(String[]args) {
+    	   
+		 {
+			
+	try {
+           int a1 = 100/2;
+           int b = 45*2;
+	}
+	catch (ArithmeticException e) {
+		
+		System.out.println((e));
+		}
+		
+ System.out.println("  Process successful : ");
+
+		 
+      
+		 }}
+ }
+ 
+		 
+       
+
+       
+ 
+       
+ 
+
+       
+
+
+
