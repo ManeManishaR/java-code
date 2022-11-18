2adab81b46deb5b6f023af36212d398ced457c3b
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-11-18 22:25:39 +0530

    Add files via upload

---
+++ hand on/Employee.java
@@ -0,0 +1,28 @@
+  
+ class Employee
+ 	{
+
+		int id;
+		String name;
+	
+
+		static String company_name;
+		
+
+		void displayData()
+
+		{
+			System.out.printIn(id + "  " + name);
+		
+	      		}
+    				static String workingDay()
+				{
+				return " Daily ";
+				}
+				 static void workingHoliday(String s);
+					{
+                                           System.out.printIn(s);
+						}
+					}
+
+		}	  
