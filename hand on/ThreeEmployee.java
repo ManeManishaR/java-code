e5412d452db1b2c7241084d403f756f7de3b8d63
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-11-18 22:34:43 +0530

    Add files via upload

---
+++ hand on/ThreeEmployee.java
@@ -0,0 +1,12 @@
+class ThreeEmployee
+       {
+         int a,b,c
+       int a= 11000;
+       int b = 5000;
+        int c 
+        }
+       Total salary for 3 employee =$11,000*3 = $33,000
+       Total salary for 2 employee =$11,000+$5000 =$ 16,000
+       Salary for the 3rd employee = $33,000 - $ 16,000 =17,000
+	
+	public static void
