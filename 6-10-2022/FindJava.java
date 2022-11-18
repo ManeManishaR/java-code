01ff96eb9127d6443ec75ebde5d8b6d8a876d1c7
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-11-18 10:38:09 +0530

    Delete 6-10-2022 directory

--- 6-10-2022/FindJava.java
+++
@@ -1,9 +0,0 @@
-package corejava;
-
-public class FindJava {
-public static void main (String[]args) {
-	//find version of java
-	System.out.println("Java version is :"+ System.getProperty("java.version"));
-	
-}
-}
