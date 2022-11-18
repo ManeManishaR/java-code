c4c2ac1442d2e0ff229b932fcaa16ec6562b8b9e
ManeManishaR <105422586+ManeManishaR@users.noreply.github.com>
2022-11-18 10:32:32 +0530

    Add files via upload

---
+++ Lab session2/3-11-2022/lab/6-10-2022/FindJava.java
@@ -0,0 +1,9 @@
+package corejava;
+
+public class FindJava {
+public static void main (String[]args) {
+	//find version of java
+	System.out.println("Java version is :"+ System.getProperty("java.version"));
+	
+}
+}
---
+++ Lab session2/3-11-2022/lab/6-10-2022/Parent.java
@@ -0,0 +1,20 @@
+
+//parent class with defult constructor and void sing method;
+// Author: Mane Manisha.R
+package corejava;
+
+public class Parent{
+	String sing;
+	
+   public Parent(String sing){
+	this.sing=sing;
+	}
+	// sing the song ;
+   public static void main(String[]args){
+		System.out.println("can i sing the song"); 
+   }
+  public void display() {
+    }
+	}	
+
+	
